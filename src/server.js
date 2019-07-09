56fd1a1bf6e159f5471fc1b7d776775d3d03128b
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2019-07-09 00:34:37 -0500

    change port already

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import notificationRoutes from "./routes/notificationRoutes";
 import seed  from "./seed"
 //settings
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3002);
 
 //Middlewares
 seed()
